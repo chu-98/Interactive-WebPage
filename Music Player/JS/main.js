287551b9cb1919c830fdc1b88f71576cb588f6dd
chu-98 <81007078+chu-98@users.noreply.github.com>
2022-04-05 17:46:12 +0900

    Music Player | 좌우 버튼 클릭 시 패널 회전시키기

--- Music Player/JS/main.js
+++ Music Player/JS/main.js
@@ -1,8 +1,45 @@
 const frame = document.querySelector("section");
-const lists = document.querySelectorAll("article");
+const lists = frame.querySelectorAll("article");
 const deg = 45; // 각각의 article 요소가 회전할 각도
 const len = lists.length - 1; // 순번이 0부터 시작하므로 전체 개수에서 1을 뺌
+const prev = document.querySelector(".btnPrev");
+const next = document.querySelector(".btnNext");
+const audio = frame.querySelectorAll("audio");
+
+let num = 0;
 let i = 0;
+let active = 0;
+
+prev.addEventListener("click", () => {
+  initMusic();
+  num++;
+  frame.style.transform = `rotate(${deg * num}deg)`;
+
+  active == 0 ? (active = len) : active--;
+  activation(active, lists);
+});
+next.addEventListener("click", () => {
+  initMusic();
+  num--;
+  frame.style.transform = `rotate(${deg * num}deg)`;
+  active == len ? (active = 0) : active++;
+  activation(active, lists);
+});
+
+function activation(index, lists) {
+  for (let el of lists) {
+    el.classList.remove("on");
+  }
+  lists[index].classList.add("on");
+}
+
+function initMusic() {
+  for (let el of audio) {
+    el.pause();
+    el.load();
+    el.parentElement.previousElementSibling.classList.remove("on");
+  }
+}
 
 // article의 개수만큼 반복
 for (let el of lists) {
@@ -13,33 +50,42 @@ for (let el of lists) {
 
   // play 버튼 클릭 시
   play.addEventListener("click", e => {
-    // play 버튼부터 .pic 요소까지 탐색한 뒤 클래스 on 추가하여 활성화
-    e.currentTarget
-      .closest("article")
-      .querySelector(".pic")
-      .classList.add("on");
-    e.currentTarget.closest("article").querySelector("audio").play();
+    let isActive = e.currentTarget.closest("article").classList.contains("on");
+    if (isActive) {
+      // play 버튼부터 .pic 요소까지 탐색한 뒤 클래스 on 추가하여 활성화
+      e.currentTarget
+        .closest("article")
+        .querySelector(".pic")
+        .classList.add("on");
+      e.currentTarget.closest("article").querySelector("audio").play();
+    }
   });
 
   // pause 버튼 클릭 시
   pause.addEventListener("click", e => {
-    // play 버튼부터 .pic 요소까지 탐색한 뒤 클래스 on 추가하여 활성화
-    e.currentTarget
-      .closest("article")
-      .querySelector(".pic")
-      .classList.remove("on");
-    e.currentTarget.closest("article").querySelector("audio").pause();
+    let isActive = e.currentTarget.closest("article").classList.contains("on");
+    if (isActive) {
+      // play 버튼부터 .pic 요소까지 탐색한 뒤 클래스 on 추가하여 활성화
+      e.currentTarget
+        .closest("article")
+        .querySelector(".pic")
+        .classList.remove("on");
+      e.currentTarget.closest("article").querySelector("audio").pause();
+    }
   });
 
   // load 버튼 클릭 시
   load.addEventListener("click", e => {
-    // play 버튼부터 .pic 요소까지 탐색한 뒤 클래스 on 추가하여 활성화
-    e.currentTarget
-      .closest("article")
-      .querySelector(".pic")
-      .classList.add("on");
-    e.currentTarget.closest("article").querySelector("audio").load();
-    e.currentTarget.closest("article").querySelector("audio").play();
+    let isActive = e.currentTarget.closest("article").classList.contains("on");
+    if (isActive) {
+      // play 버튼부터 .pic 요소까지 탐색한 뒤 클래스 on 추가하여 활성화
+      e.currentTarget
+        .closest("article")
+        .querySelector(".pic")
+        .classList.add("on");
+      e.currentTarget.closest("article").querySelector("audio").load();
+      e.currentTarget.closest("article").querySelector("audio").play();
+    }
   });
 
   // 각 article 요소를 45도씩 회전하고 아래로 배치
