8dc1a0e4a643a77849fec7a456181878d9e42b55
chu-98 <81007078+chu-98@users.noreply.github.com>
2022-04-05 17:09:33 +0900

    Music Player | 컨트롤 패널 버튼으로 음악을 재생 및 정지하기

--- Music Player/JS/main.js
+++ Music Player/JS/main.js
@@ -7,6 +7,41 @@ let i = 0;
 // article의 개수만큼 반복
 for (let el of lists) {
   let pic = el.querySelector(".pic");
+  let play = el.querySelector(".play");
+  let pause = el.querySelector(".pause");
+  let load = el.querySelector(".load");
+
+  // play 버튼 클릭 시
+  play.addEventListener("click", e => {
+    // play 버튼부터 .pic 요소까지 탐색한 뒤 클래스 on 추가하여 활성화
+    e.currentTarget
+      .closest("article")
+      .querySelector(".pic")
+      .classList.add("on");
+    e.currentTarget.closest("article").querySelector("audio").play();
+  });
+
+  // pause 버튼 클릭 시
+  pause.addEventListener("click", e => {
+    // play 버튼부터 .pic 요소까지 탐색한 뒤 클래스 on 추가하여 활성화
+    e.currentTarget
+      .closest("article")
+      .querySelector(".pic")
+      .classList.remove("on");
+    e.currentTarget.closest("article").querySelector("audio").pause();
+  });
+
+  // load 버튼 클릭 시
+  load.addEventListener("click", e => {
+    // play 버튼부터 .pic 요소까지 탐색한 뒤 클래스 on 추가하여 활성화
+    e.currentTarget
+      .closest("article")
+      .querySelector(".pic")
+      .classList.add("on");
+    e.currentTarget.closest("article").querySelector("audio").load();
+    e.currentTarget.closest("article").querySelector("audio").play();
+  });
+
   // 각 article 요소를 45도씩 회전하고 아래로 배치
   el.style.transform = `rotate(${deg * i}deg) translateY(-90vh)`;
   pic.style.backgroundImage = `url(img/member${
