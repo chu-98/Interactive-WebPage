66c59ccfe153459850d10c9f06b1864aa468e1e5
chu-98 <81007078+chu-98@users.noreply.github.com>
2022-04-07 16:01:00 +0900

    Reactive Web Gallery | 함수를 이용해 자바스크립트 코드 기능별로 개선하기 = 마무리

--- Reactive Web Gallery/JS/main.js
+++ Reactive Web Gallery/JS/main.js
@@ -1,32 +1,44 @@
+const frame = "section";
+const box = "article";
+const speed = "0.5s";
+const activeClass = "on";
+const btn = document.querySelectorAll(".menu li");
+let grid;
+
 window.addEventListener("load", () => {
-  const grid = new Isotope("section", {
-    itemSelector: "article",
-    columnWidth: "article",
-    transitionDuration: "0.5s",
+  init(); // 화면 초기화 함수 호출
+  filter(btn); // 정렬 버튼 기능의 함수 호출
+});
+
+// 화면 초기화 함수 정의
+function init() {
+  // 변수 grid에 담길 결괏값이 다른 함수인 filter에서도 활용되어야 하므로
+  // 전역 변수로 선언
+  grid = new Isotope(frame, {
+    itemSelector: box,
+    columnWidth: box,
+    transitionDuration: speed,
   });
+}
 
-  const btns = document.querySelectorAll("main ul li");
-
-  for (let el of btns) {
+// 정렬 버튼 기능의 함수 정의
+function filter(arr) {
+  for (let el of arr) {
     el.addEventListener("click", e => {
       e.preventDefault();
 
-    // 변수 sort에 클릭한 대상의 자식인 a 요소의 href 속성값 저장
-    const sort = e.currentTarget.querySelector("a").getAttribute("href");
+      // 변수 sort에 클릭한 대상의 자식인 a 요소의 href 속성값 저장
+      const sort = e.currentTarget.querySelector("a").getAttribute("href");
 
-    // grid에 저장된 결괏값을 불러와 재정렬 기능 연결
-    grid.arrange({
-      filter: sort
+      // grid에 저장된 결괏값을 불러와 재정렬 기능 연결
+      grid.arrange({
+        filter: sort,
+      });
+
+      for (let el of arr) {
+        el.classList.remove(activeClass);
+      }
+      e.currentTarget.classList.add(activeClass);
     });
-
-    // 다시 모든 버튼의 개수만큼 반복해서
-    for(let of btns) {
-      // 각 버튼의 클래스명 'on'을 제거해 비활성화
-      el.classList.remove(on);
-    }
-
-    // 클릭한 대상만 선택해서 클래스명 on을 추가해 활성화
-    e.currentTarget.classList.add("on");
-    })
   }
-});
+}
